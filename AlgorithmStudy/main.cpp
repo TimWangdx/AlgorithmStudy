3b6ef3678d802792f17ccbe3f79b612114bff7b7
wang_wonderful <wang_wonderful@foxmail.com>
2017-02-05 09:45:57 +0800

    Initial Commit

---
+++ AlgorithmStudy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgorithmStudy
+//
+//  Created by HaKim on 17/2/5.
+//  Copyright © 2017年 haKim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
